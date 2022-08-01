7391791374078568a43d6d27bc8a5f637883ab44
JiWoong Choi <99054659+zwoong@users.noreply.github.com>
2022-08-01 14:18:02 +0900

    [Bronze V] Title: 학점계산, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+C = input()
+
+if C == "A+":
+    print(4.3)
+elif C == "A0":
+    print(4.0)
+elif C == "A-":
+    print(3.7)
+elif C == "B+":
+    print(3.3)
+elif C == "B0":
+    print(3.0)
+elif C == "B-":
+    print(2.7)
+elif C == "C+":
+    print(2.3)
+elif C == "C0":
+    print(2.0)
+elif C == "C-":
+    print(1.7)
+elif C == "D+":
+    print(1.3)
+elif C == "D0":
+    print(1.0)
+elif C == "D-":
+    print(0.7)
+elif C == "F":
+    print(0.0)
