7e0dc8618f0040250a6cbd78ab7301a11e79a61d
JiWoong Choi <99054659+zwoong@users.noreply.github.com>
2022-08-01 14:33:03 +0900

    [Bronze III] Title: TGN, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for i in range(T):
+    r, e, c = map(int, input().split())
+    if e > r + c:
+        print("advertise")
+    elif e == r + c:
+        print("does not matter")
+    else:
+        print("do not advertise")
