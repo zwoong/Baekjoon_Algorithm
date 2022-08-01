3ef51d49f88e69387a93d0289e1f067411991cf1
JiWoong Choi <99054659+zwoong@users.noreply.github.com>
2022-08-01 15:10:15 +0900

    [Bronze III] Title: 배수와 약수, Time: 64 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,11 @@
+status = True
+while status:
+    A, B = map(int, input().split())
+    if A == 0 and B == 0:
+        break
+    elif B % A == 0:
+        print("factor")
+    elif A % B == 0:
+        print("multiple")
+    else:
+        print("neither")
