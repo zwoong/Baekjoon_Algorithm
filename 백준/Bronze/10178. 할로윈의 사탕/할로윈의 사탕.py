ce714c89004e936afa2b891e5ff4a461d5f16b85
JiWoong Choi <99054659+zwoong@users.noreply.github.com>
2022-08-16 17:34:50 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.py
@@ -0,0 +1,8 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    c, v = map(int, input().split())
+    print("You get {0} piece(s) and your dad gets {1} piece(s).".format(c // v, c % v))
